8c7b75079efca863baacdaf800a5a826e332c740
Onion2Bolt <99247726+Onion2Bolt@users.noreply.github.com>
2022-05-09 23:15:24 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+    public class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+class Hashtable {
+
+    BinarySearchTree bst;
+
+    public void Hashtable() {
+        bst = new BinarySearchTree();
+    }
+
+}
